80aa4b847dcb344ed1b6a0b3596ed4870738f9e3
Jeroen Gunster <jeroen.gunster@gmx.com>
2022-09-14 00:06:03 +0200

    chore: grid + square select

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,14 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'prettier'],
-	plugins: ['svelte3'],
+	extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
+	plugins: ['prettier', 'svelte3'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3'
+		}
+	],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
 	env: {
 		browser: true,
 		es2017: true,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import P5 from 'p5-svelte';
+
+	const squareSide = 30;
+
+	const sketch = (p5) => {
+		p5.setup = () => {
+			p5.createCanvas(p5.windowWidth, p5.windowHeight);
+		};
+
+		p5.draw = () => {
+			for (let x = 1; x < 21; x++) {
+				for (let y = 1; y < 21; y++) {
+					selectSquare(x, y);
+					p5.square(squareSide * x, squareSide * y, squareSide);
+				}
+			}
+		};
+
+		p5.windowResized = () => {
+			p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+		};
+
+		const selectSquare = (x, y) => {
+			p5.mouseX > squareSide * x &&
+			p5.mouseX < squareSide * x + squareSide &&
+			p5.mouseY > squareSide * y &&
+			p5.mouseY < squareSide * y + squareSide
+				? p5.fill(175)
+				: p5.fill(255);
+		};
+	};
+</script>
+
+<P5 {sketch} />
