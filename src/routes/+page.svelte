363782cef86713b901bf1606742e6cc599b4ee89
Jeroen Gunster <jeroen.gunster@gmx.com>
2022-09-14 00:22:08 +0200

    chore: give board default tints

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 		p5.draw = () => {
 			for (let x = 1; x < 21; x++) {
 				for (let y = 1; y < 21; y++) {
-					selectSquare(x, y);
+					selectSquare(x, y, getDefaultTint(x, y));
 					p5.square(squareSide * x, squareSide * y, squareSide);
 				}
 			}
@@ -21,13 +21,23 @@
 			p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
 		};
 
-		const selectSquare = (x, y) => {
+		const getDefaultTint = (x, y) => {
+			let tint;
+			if (x % 2 === 0) {
+				y % 2 === 0 ? (tint = 255) : (tint = 200);
+			} else if (x % 2 !== 0) {
+				y % 2 === 0 ? (tint = 200) : (tint = 255);
+			}
+			return tint;
+		};
+
+		const selectSquare = (x, y, defaultTint) => {
 			p5.mouseX > squareSide * x &&
 			p5.mouseX < squareSide * x + squareSide &&
 			p5.mouseY > squareSide * y &&
 			p5.mouseY < squareSide * y + squareSide
-				? p5.fill(175)
-				: p5.fill(255);
+				? p5.fill(125)
+				: p5.fill(defaultTint);
 		};
 	};
 </script>
